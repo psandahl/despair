e7d2b4f7998d46c2d29b3914ee230299fadd42c9
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-12-22 16:58:43 +0100

    Windowed Fourier Transform filter.

--- despair/filter.py
+++ despair/filter.py
@@ -1,6 +1,15 @@
 import cmath
 import numpy as np
 
+"""
+Filters are taken from:
+
+Linkoping Studies in Science and Technology. Dissertations No. 379
+Focus of Attention and Gaze Control for Robot Vision
+
+Carl-Johan Westelius
+"""
+
 
 def cos2(x: float) -> float:
     """
@@ -31,3 +40,19 @@ def nonring(r: float) -> np.ndarray:
     x_pi = x * np.pi
 
     return cos2(x_pi / r2) * np.exp(-1j * (x_pi / r + np.sin(x_pi / r)))
+
+
+def wft(r: float) -> np.ndarray:
+    """
+    Compute a discrete, complex windowed fourier transform 
+    filter with the given radius.
+
+    Parameters:
+        r: Radius of the filter.
+
+    Returns:
+        The filter coefficients, in a complex numpy array.
+    """
+    x = np.arange(-r, r + 1, dtype=np.float64)
+
+    return np.exp(-1j * np.pi * (x / r))
--- despair/filter_plot.py
+++ despair/filter_plot.py
@@ -14,26 +14,28 @@ def plot_with_radius(r: float) -> None:
     x = np.arange(-r, r + 1, dtype=np.float64)
 
     filters = [
-        ("Nonring", filter.nonring)
+        ("Nonring", filter.nonring),
+        ("Windowed FT", filter.wft)
     ]
 
-    fig = plt.figure(figsize=(8, 2 * len(filters)))
+    num_filters = len(filters)
+    fig = plt.figure(figsize=(8, num_filters * 2))
 
     idx = 1
     for label, func in filters:
         filt = func(r)
 
-        ax1 = fig.add_subplot(1, 3, idx)
+        ax1 = fig.add_subplot(num_filters, 3, idx)
         ax1.plot(x, filt.real, color='#0000ff',
                  linewidth=2)
-        ax1.set_title(f'{label} real part')
+        ax1.set_title(f'{label} re/even part')
 
-        ax2 = fig.add_subplot(1, 3, idx + 1)
+        ax2 = fig.add_subplot(num_filters, 3, idx + 1)
         ax2.plot(x, filt.imag, color='#00ff00',
                  linewidth=2)
-        ax2.set_title(f'{label} imaginary part')
+        ax2.set_title(f'{label} im/odd part')
 
-        ax3 = fig.add_subplot(1, 3, idx + 2)
+        ax3 = fig.add_subplot(num_filters, 3, idx + 2)
         ax3.plot(x, filt.real, color='#0000ff',
                  linewidth=2)
         ax3.plot(x, filt.imag, color='#00ff00',
