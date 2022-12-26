8e45a3db4a13b902a7d97d89abaccbbafaa45ac4
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-12-26 19:02:59 +0100

    Better comments.

--- despair/filter.py
+++ despair/filter.py
@@ -2,26 +2,18 @@ import numpy as np
 import scipy.signal as signal
 
 """
-Filters are taken from:
+Filters are inspired by:
 
 Linkoping Studies in Science and Technology. Dissertations No. 379
-Focus of Attention and Gaze Control for Robot Vision
+Focus of Attention and Gaze Control for Robot Vision.
 
-Carl-Johan Westelius
+The filters respond to line and edge events like the following:
+
+Magnitude is significant greater than zero.
+
+Phase is zero for light line, is pi (or -pi) for dark line, pi/2 for
+dark to light edge and -pi/2 for light to dark edge.
 """
-
-
-def cos2(x: float) -> float:
-    """
-    Compute cos2.
-
-    Parameters:
-        x: Value to cos.
-
-    Returns:
-        The cos2 value.
-    """
-    return np.cos(x) ** 2.0
 
 
 def nonring(r: float) -> np.ndarray:
@@ -41,7 +33,7 @@ def nonring(r: float) -> np.ndarray:
     x = np.arange(-r, r + 1, dtype=np.float64)
     x_pi = x * np.pi
 
-    return cos2(x_pi / r2) * np.exp(-1j * (x_pi / r + np.sin(x_pi / r)))
+    return __cos2(x_pi / r2) * np.exp(-1j * (x_pi / r + np.sin(x_pi / r)))
 
 
 def wft(r: float) -> np.ndarray:
@@ -78,3 +70,16 @@ def convolve(data: np.ndarray, filter: np.ndarray) -> np.ndarray:
     assert len(data) >= len(filter)
 
     return signal.convolve(data, filter, mode='same')
+
+
+def __cos2(x: float) -> float:
+    """
+    Compute cos2.
+
+    Parameters:
+        x: Value to cos.
+
+    Returns:
+        The cos2 value.
+    """
+    return np.cos(x) ** 2.0
