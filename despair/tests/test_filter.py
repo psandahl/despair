531b9fdb672514c1445797ccd319faeeb230bdf5
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-12-27 18:34:09 +0100

    Unit tests for edges.

--- despair/tests/test_filter.py
+++ despair/tests/test_filter.py
@@ -1,5 +1,7 @@
 import despair.filter as filter
+import despair.util as util
 
+import math
 import numpy as np
 import unittest
 
@@ -45,13 +47,19 @@ class FilterTest(unittest.TestCase):
 
     def test_nonring_line_response(self):
         """
-        Test nonring responses for lines.
+        Test line responses for nonring.
         """
         self.__line_response(self.nonring_filters, 'nonring')
 
+    def test_nonring_edge_response(self):
+        """
+        Test edge responses for nonring.
+        """
+        self.__edge_response(self.nonring_filters, 'nonring')
+
     def test_wft_line_response(self):
         """
-        Test WFT responses for lines.
+        Test line responses for WFT.
         """
         self.__line_response(self.wft_filters, 'wft')
 
@@ -76,5 +84,32 @@ class FilterTest(unittest.TestCase):
                 # Phase shall be equal to the goal.
                 self.assertAlmostEqual(
                     goal, abs(ph), places=5, msg=f'Filter={label} radius={r}')
+
+            r += 1
+
+    def __edge_response(self, filters: np.ndarray, label: str):
+        r = 3
+        for filt in filters:
+            response = filter.convolve(self.signal, filt)
+
+            # For edges in the test signal the exact response lies in between
+            # the pixels, and has to be interpolated.
+            magnitude = np.abs(response)
+            phase = np.angle(response)
+            mean = np.mean(magnitude)
+
+            self.assertGreater(mean, 0.0, msg=f'Filter={label}')
+
+            for index0, index1, goal in [(59, 60, math.pi / 2), (140, 141, -math.pi / 2)]:
+                mag = util.mix(magnitude[index0], magnitude[index1], 0.5)
+                ph = util.mix(phase[index0], phase[index1], 0.5)
+
+                # Magnitude shall be greater than mean. Todo: Find peaks.
+                self.assertGreater(
+                    mag, mean, msg=f'Filter={label} radius={r}')
+
+                # Phase shall be equal to the goal.
+                self.assertAlmostEqual(
+                    goal, ph, places=2, msg=f'Filter={label} radius={r}')
 
             r += 1
--- despair/util.py
+++ despair/util.py
@@ -2,6 +2,21 @@ import numpy as np
 import scipy.ndimage as ndimage
 
 
+def mix(x0: float, x1: float, amount: float) -> float:
+    """
+    Interpolate between two values.
+
+    Parameters:
+        x0: The first value.
+        x1: The second value.
+        amount: The mix factor 0 - 1, tells how much to take from x1.
+
+    Returns:
+        The interpolated value.
+    """
+    return x0 * (1.0 - amount) + x1 * amount
+
+
 def feature_image(blur: bool = False) -> np.ndarray:
     """
     Create a 160x160 feature image with:
