11ebf0c7a65f694c09665e0730c58149eb6ef60f
Patrik Sandahl <patrik.sandahl@gmail.com>
2022-12-27 22:37:10 +0100

    Improved unit tests.

--- despair/tests/test_filter.py
+++ despair/tests/test_filter.py
@@ -3,7 +3,12 @@ import despair.util as util
 
 import math
 import numpy as np
+import scipy.signal as signal
 import unittest
+
+
+def is_peak(peaks: np.ndarray, target: int, margin: int = 0) -> bool:
+    return np.min(np.abs(peaks - target)) <= margin
 
 
 class FilterTest(unittest.TestCase):
@@ -50,15 +55,22 @@ class FilterTest(unittest.TestCase):
 
             self.assertGreater(mean, 0.0)
 
+            # Get the magnitude peaks.
+            peaks, _ = signal.find_peaks(magnitude)
+
+            # Hack warning ...
+            self.assertLessEqual(len(peaks), 6)
+
             for index, goal in [(20, 0.0), (100, 3.14159)]:
                 mag = magnitude[index]
                 ph = phase[index]
 
-                # Magnitude shall be greater than mean. Todo: Find peaks.
-                self.assertGreater(mag, mean)
+                # The target index shall be a peak in the magnitude.
+                self.assertTrue(is_peak(peaks, index, 2), msg=f'radius={r}')
 
                 # Phase shall be equal to the goal.
-                self.assertAlmostEqual(goal, abs(ph), places=5)
+                self.assertAlmostEqual(
+                    goal, abs(ph), places=5, msg=f'radius={r}')
 
             r += 1
 
@@ -76,14 +88,20 @@ class FilterTest(unittest.TestCase):
 
             self.assertGreater(mean, 0.0)
 
+            # Get the magnitude peaks.
+            peaks, _ = signal.find_peaks(magnitude)
+
+            # Hack warning ...
+            self.assertLessEqual(len(peaks), 6)
+
             for index0, index1, goal in [(59, 60, math.pi / 2), (140, 141, -math.pi / 2)]:
                 # For edges in the test signal the exact response lies in between
                 # the pixels, and has to be interpolated.
                 mag = util.mix(magnitude[index0], magnitude[index1], 0.5)
                 ph = util.mix(phase[index0], phase[index1], 0.5)
 
-                # Magnitude shall be greater than mean. Todo: Find peaks.
-                self.assertGreater(mag, mean)
+                # The target index shall be a peak in the magnitude.
+                self.assertTrue(is_peak(peaks, index0, 1), msg=f'radius={r}')
 
                 # Phase shall be equal to the goal.
                 self.assertAlmostEqual(goal, ph, places=2)
